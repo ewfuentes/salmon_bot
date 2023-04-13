4006f50de936204c0957c834987c8c9f1b0b38e5
Erick Fuentes <fuentes.erick@gmail.com>
2023-04-12 22:09:12 -0400

    finish defining geometry

--- salmonbot/main.py
+++ salmonbot/main.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 import argparse
 
-from pydrake.all import MultibodyPlant, MeshcatVisualizer, Parser, ModelVisualizer, Simulator, StartMeshcat
+from pydrake.all import (
+    AddMultibodyPlantSceneGraph,
+    ModelVisualizer,
+    StartMeshcat,
+    DiagramBuilder,
+    Parser,
+)
+
+
+def load_model(world_path: str, robot_path: str):
+    builder = DiagramBuilder()
+    robot, robot_scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
+    Parser(robot).AddModelFromFile(robot_path, model_name="Robot")
+    Parser(robot).AddModelFromFile(world_path, model_name="World")
 
 
 def run(world_path: str, robot_path: str):
-    print('Loading model from', world_path)
-    print('Loading model from', robot_path)
-
+    load_model(world_path, robot_path)
     meshcat = StartMeshcat()
     visualizer = ModelVisualizer(meshcat=meshcat)
     visualizer.AddModels(world_path)
@@ -17,10 +28,8 @@ def run(world_path: str, robot_path: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--robot', help='path to robot URDF model', required=True)
-    parser.add_argument(
-        '--world', help='path to world URDF model', required=True)
+    parser.add_argument("--robot", help="path to robot URDF model", required=True)
+    parser.add_argument("--world", help="path to world URDF model", required=True)
 
     args = parser.parse_args()
 
